2febe268f427a9f4ead65a07d9a2be5f091c3563
RobinFrugte97 <robin.frugte@hotmail.com>
2021-05-27 16:55:25 +0200

    fixed sw for portfolio

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ const app = new App({
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener("load", function () {
-		navigator.serviceWorker.register('/sw.js')
+		navigator.serviceWorker.register('./sw.js')
 			.then(function (registration) {
 				return registration.update();
 			})
