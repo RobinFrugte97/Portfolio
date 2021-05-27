7e1d68ff4884c87f16e831ed5d941c5c9ebc0412
RobinFrugte97 <robin.frugte@hotmail.com>
2021-05-27 16:29:46 +0200

    Tweaked manifest

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -22,10 +22,10 @@
 	</div>
 
 	<nav class="desktop">
-		<a href="/#/projects" on:click={() => {
+		<a href="/Portfolio/#/projects" on:click={() => {
 			selectedColor.set("")
 		}}>Projects</a>
-		<a href="/#/aboutme">About</a>
+		<a href="/Portfolio/#/aboutme">About</a>
 	</nav>
 </header>
 <header id="mobile" class="mobile">
@@ -34,12 +34,12 @@
 		menu.style = "animation: removeMenu .8s forwards ease;"
 	}}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"/></svg></button>
 	<nav>
-		<a href="/#/projects" on:click={() => {
+		<a href="/Portfolio/#/projects" on:click={() => {
 			selectedColor.set("")
 			let menu = document.getElementById("mobile")
 			menu.style = "animation: removeMenu .8s forwards ease;"
 		}}>projects</a>
-		<a href="/#/aboutme" on:click={() => {
+		<a href="/Portfolio/#/aboutme" on:click={() => {
 		let menu = document.getElementById("mobile")
 		menu.style = "animation: removeMenu .8s forwards ease;"
 	}}>about</a>
